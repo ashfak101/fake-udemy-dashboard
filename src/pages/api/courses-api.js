7df8e9a64ebddb3984964c3a91da79735e07539f
ashfak101 <ashfakulkarim2@gmail.com>
2022-05-23 10:21:12 +0600

    Quiz Data Created

--- src/pages/api/courses-api.js
+++ src/pages/api/courses-api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const handler= async(req, res)=>{
     try{
-        const {data}=await axios.get('https://jsonkeeper.com/b/UFAZ')
+        const {data}=await axios.get('https://jsonkeeper.com/b/85I9')
         res.send(data)
     }
     catch(err){
