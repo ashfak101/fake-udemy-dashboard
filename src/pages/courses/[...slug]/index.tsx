bb9127d584dd36c7bfd923b86c5f225b83cb0e0f
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-05-27 15:34:37 +0600

    MouseOver event left right button done

--- src/pages/courses/[...slug]/index.tsx
+++ src/pages/courses/[...slug]/index.tsx
@@ -1,12 +1,12 @@
-import { Box, Button } from '@mui/material';
+import { Box, Button, IconButton, Tooltip } from '@mui/material';
 import CourseContent from 'components/course/courseContent';
 import LessonDetails from 'components/course/LessonDetails';
 import CourseNav from 'components/shared/CourseNav';
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { CourseInterFace, MainCourse, Module } from '../../../components/types'
-
-
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 const Index = () => {
   const router = useRouter();
   const { slug }: any = router.query
@@ -17,7 +17,10 @@ const Index = () => {
   const [moduleId, setModuleId] = useState<any>()
   const [module, setModule] = useState<Module>()
   const [progress, setProgress] = useState<number>(0)
-const [ totallesson,setTotalLesson]=useState<number>(0)
+  const [totallesson, setTotalLesson] = useState<number>(0)
+  const[rightMouseData,setRightMouseData]=useState<Module>()
+  const [leftMouseData,setLeftMouseData]=useState<Module>()
+
   useEffect(() => {
     fetch('/api/courses-api')
       .then(res => res.json())
@@ -40,9 +43,9 @@ const [ totallesson,setTotalLesson]=useState<number>(0)
     setTotalLesson(a)
 
 
-  }, [courses, slug, courseName,allCourse])
+  }, [courses, slug, courseName, allCourse])
 
-  const singleLesson = allCourse?.find(courses=>courses.module.find(mod=> mod.id == moduleId))
+  const singleLesson = allCourse?.find(courses => courses.module.find(mod => mod.id == moduleId))
 
   const singleModule = singleLesson?.module.find(module => module.id == moduleId)
 
@@ -52,55 +55,92 @@ const [ totallesson,setTotalLesson]=useState<number>(0)
       if (index == 0) {
         setCourseName(ele)
       }
-     
+
       if (index == 3) {
         setModuleId(ele)
       }
     })
     setModule(singleModule)
   }, [slug, url, singleModule])
-  
+
   const handleNext = () => {
-    let a =0
-    allCourse?.forEach(course =>{
-       a+= course.length
+    let a = 0
+    allCourse?.forEach(course => {
+      a += course.length
     })
     console.log(a);
-    if(moduleId <a ){
-      let id= parseInt(moduleId)+1
+    if (moduleId < a) {
+      let id = parseInt(moduleId) + 1
       router.push(`/courses/web-design-secrets/learn/lecture/${id}`)
     }
-      
+
   }
-const handleBack=()=>{
-  let a =0
-  allCourse?.forEach(course =>{
-     a+= course.length
-  })
-  if(moduleId > 1 && moduleId <= a){
-    let id:number = parseInt(moduleId) - 1
-    router.push(`/courses/web-design-secrets/learn/lecture/${id}`)
-   }
-  //   else if (moduleId >4 ){
-  //   let id:number = parseInt(moduleId) - 1
-  //   router.push(`/courses/web-design-secrets/learn/lesson2/${id}`)
-  //  }
-}
+  const handleBack = () => {
+    let a = 0
+    allCourse?.forEach(course => {
+      a += course.length
+    })
+    if (moduleId > 1 && moduleId <= a) {
+      let id: number = parseInt(moduleId) - 1
+      router.push(`/courses/web-design-secrets/learn/lecture/${id}`)
+    }
+
+  }
+  const handleNextLesson =()=>{
+
+    let a = 0
+    allCourse?.forEach(course => {
+      a += course.length
+    })
+    if (moduleId < a){
+      let id:number= parseInt(moduleId) + 1;
+      console.log(id);
+      const singleLesson = allCourse?.find(courses => courses.module.find(mod => mod.id == id))
+    const singleModule = singleLesson?.module.find(module => module.id == id)
+    setRightMouseData(singleModule)
+    }
+    
+  }
+  const handlePrevLesson=()=>{
+    let a = 0
+    allCourse?.forEach(course => {
+      a += course.length
+    })
+    if (moduleId > 1 && moduleId <= a) {
+      let num: number = parseInt(moduleId) - 1
+      const singleLesson = allCourse?.find(courses => courses.module.find(mod => mod.id == num))
+      const singleModule = singleLesson?.module.find(module => module.id == num)
+      setLeftMouseData(singleModule)
+    }
+    
+  }
+  console.log(singleModule)
   return (
     <>
-      <CourseNav progress={progress} totallesson={totallesson}/>
+      <CourseNav progress={progress} totallesson={totallesson} />
       <Box sx={{
         display: 'flex', flexDirection: {
           xs: 'column', md: 'row'
         }
       }}>
-        <Box sx={{ width: { xs: '100%', md: '70%' } }}>
+        <Box sx={{ width: { xs: '100%', md: '70%', position: 'relative' } }}>
           <LessonDetails module={module}></LessonDetails>
-          <Box  >
-            <Button onClick={handleNext}>NEXT</Button>
+          {/* rightMouseData */}
+          <Box  onMouseOver={handleNextLesson} sx={{ position: 'absolute', left: { xs: "94%", md: '873px', lg: "1318px" }, top: "40%", }}>
+            <Tooltip  title={`${rightMouseData?.id}.${rightMouseData?.lessonTitle}`} placement="left">
+              <IconButton sx={{ background: "#333", borderRadius: '0', px: '0', color: '#fff','&:hover': { background: '#999',color:'#fff' }  }} onClick={handleNext}>
+                <ArrowForwardIosIcon />
+              </IconButton>
+            </Tooltip>
           </Box>
-          <Box  >
-            <Button onClick={handleBack}>Back</Button>
+          {/* leftMouseData */}
+          <Box onMouseOver={handlePrevLesson} sx={{ position: 'absolute', left: 0, top: "40%" }}>
+            <Tooltip title={`${leftMouseData?.id}.${leftMouseData?.lessonTitle}`} placement="right">
+              <IconButton sx={{ background: "#333", borderRadius: '0', px: '0', color: '#fff','&:hover': { background: '#999',color:'#fff' } }} onClick={handleBack}>
+                <ArrowBackIosNewIcon />
+              </IconButton>
+            </Tooltip>
+
           </Box>
         </Box>
         <Box sx={{ width: { xs: '100%', md: '30%' }, background: '#f2f7f6' }}>
@@ -113,6 +153,7 @@ const handleBack=()=>{
           }
         </Box>
       </Box>
+
     </>
   )
 }
